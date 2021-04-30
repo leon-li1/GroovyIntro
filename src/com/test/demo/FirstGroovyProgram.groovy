8b98064d942743d8e92bdb76ace1a5859b3f1a45
Alac Wong <46137625+alacwong@users.noreply.github.com>
2021-04-30 13:45:31 -0400

    Update FirstGroovyProgram.groovy

--- src/com/test/demo/FirstGroovyProgram.groovy
+++ src/com/test/demo/FirstGroovyProgram.groovy
@@ -2,16 +2,6 @@ package com.test.demo
 
 class FirstGroovyProgram {
     static void main(String[] args) {
-        Integer age = 34
-        println("Hello $age!")
-        5.times { print it }
-        println()
-        def lis = [1, 2, 3, 4, 5]
-        lis.each { print it }
-        println()
-        def m = ["a": 1, "b": 2]
-        def m2 = m.entrySet()
-        println m2.getClass()
-        println(m2)
+        println("n")
     }
 }
